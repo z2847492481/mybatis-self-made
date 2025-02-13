3b0dd0837ecd1b5c0516d6d60199f46f608b8073
zhqlxl <z465719151@outlook.com>
2025-02-13 22:24:18 +0800

    删除多余代码

--- src/main/java/com/zhq/session/defaults/DefaultSqlSession.java
+++ src/main/java/com/zhq/session/defaults/DefaultSqlSession.java
@@ -44,34 +44,6 @@ public class DefaultSqlSession implements SqlSession {
         return list.get(0);
     }
 
-    private <T> List<T> resultSet2Obj(ResultSet resultSet, Class<?> clazz) {
-        List<T> list = new ArrayList<>();
-        try {
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            // 每次遍历行值
-            while (resultSet.next()) {
-                T obj = (T) clazz.newInstance();
-                for (int i = 1; i <= columnCount; i++) {
-                    Object value = resultSet.getObject(i);
-                    String columnName = metaData.getColumnName(i);
-                    String setMethod = "set" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
-                    Method method;
-                    if (value instanceof Timestamp) {
-                        method = clazz.getMethod(setMethod, Date.class);
-                    } else {
-                        method = clazz.getMethod(setMethod, value.getClass());
-                    }
-                    method.invoke(obj, value);
-                }
-                list.add(obj);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
     @Override
     public Object getMapper(Class type) {
         return configuration.getMapper(type, this);
